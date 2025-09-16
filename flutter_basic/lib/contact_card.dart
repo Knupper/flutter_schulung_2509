4759e4bfd0cff53ea2b2650669990dc443083655
Max Steffen <max.steffen@mail.com>
2025-09-16 09:45:15 +0200

    add image asset and network

--- flutter_basic/lib/contact_card.dart
+++ flutter_basic/lib/contact_card.dart
@@ -13,11 +13,20 @@ class ContactCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(border: Border.all(), color: Colors.grey[200], borderRadius: BorderRadius.circular(16)),
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         spacing: 8,
         children: [
-          Placeholder(fallbackHeight: 100, fallbackWidth: 100),
+          // Image.asset('images/sticker.png', fit: BoxFit.scaleDown, height: 100, width: 100),
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(image: NetworkImage('https://picsum.photos/200')),
+            ),
+          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
